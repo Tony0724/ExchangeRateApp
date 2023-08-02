8de4d98d81bc3cdfc197eacf896895058adb6ecf
Tony0724 <0724ksm@naver.com>
2023-08-02 11:39:26 +0900

    change api

--- script.js
+++ script.js
@@ -1,97 +1,93 @@
-const API_KEY = 'fed5374afcb52cdd9d20';
-const API_BASE_URL = 'https://free.currconv.com/api/v7';
+// HTML 요소를 가져옵니다.
+const fromCurrencySelect = document.getElementById('fromCurrency');
+const toCurrencySelect = document.getElementById('toCurrency');
+const amountInput = document.getElementById('amount');
+const resultText = document.getElementById('result');
 
-const currencyConverter = axios.create({
-  baseURL: API_BASE_URL,
-  params: {
-    apiKey: API_KEY,
-  },
-});
+// 환율 정보를 가져오고 select 옵션을 설정합니다.
+fetch('https://api.manana.kr/exchange.json')
+  .then(response => response.json())
+  .then(data => {
+    const result = data.map(item => {
+      if (item.symbol.includes('=X')) {
+        item.symbol = item.symbol.replace('=X', '');
+      }
+      return item;
+    });
 
-const getAllCurrencies = async () => {
-  try {
-    const response = await currencyConverter.get('/currencies');
-    return response.data.results;
-  } catch (error) {
-    console.error('Error fetching currencies:', error);
-    throw error;
-  }
-};
+    const options = result.map(item => {
+      const value = item.symbol.replace('USD/', '');
+      let text = '';
+      if (item.kr === undefined) {
+        text = item.symbol + ' ' + '-정보가 없습니다.';
+      } else {
+        text = item.kr + ' ' + item.symbol;
+      }
+      return { value, text };
+    });
 
-const getExchangeRate = async (fromCurrency, toCurrency) => {
-    try {
-        const response = await currencyConverter.get('/convert', {
-          params: {
-            q: `${fromCurrency}_${toCurrency}`,
-            compact: 'ultra',
-          },
+    options.sort((a, b) => a.text.localeCompare(b.text, 'ko-KR', { sensitivity: 'base' }));
+
+    // fromCurrency와 toCurrency 옵션 설정
+    options.forEach(option => {
+      const optionElement = document.createElement('option');
+      optionElement.value = option.value;
+      optionElement.text = option.text;
+      if (option.value === 'SGD') {
+        optionElement.selected = true;
+      }
+      fromCurrencySelect.appendChild(optionElement);
+    });
+
+    options.forEach(option => {
+      const optionElement = document.createElement('option');
+      optionElement.value = option.value;
+      optionElement.text = option.text;
+      if (option.value === 'KRW') {
+        optionElement.selected = true;
+      }
+      toCurrencySelect.appendChild(optionElement);
+    });
+
+    // 환율 정보를 업데이트하는 함수
+    const updateExchangeRate = () => {
+      const fromCurrency = fromCurrencySelect.value;
+      const toCurrency = toCurrencySelect.value;
+      const amount = parseFloat(amountInput.value);
+
+      if (isNaN(amount)) {
+        resultText.innerText = 'Please enter a valid amount';
+        return;
+      }
+
+      const url = `https://api.manana.kr/exchange/rate.json?base=${toCurrency}&code=${fromCurrency}`;
+
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          const exchangeRate = data[0].rate;
+          const convertedAmount = (amount * exchangeRate).toFixed(2);
+          resultText.innerText = `${amount} ${fromCurrency} = ${convertedAmount} ${toCurrency}`;
+        })
+        .catch(error => {
+          console.error('Error fetching exchange rate:', error);
+          resultText.innerText = 'Error fetching exchange rate. Please try again later.';
         });
-        return response.data[`${fromCurrency}_${toCurrency}`];
-      } catch (error) {
-        console.error('Error fetching exchange rate:', error);
-        throw error;
-      }
-};
+    };
 
-const convertCurrency = async () => {
-  try {
-    const fromCurrency = document.getElementById('fromCurrency').value;
-    const toCurrency = document.getElementById('toCurrency').value;
-    const amount = parseFloat(document.getElementById('amount').value);
+    // Convert 버튼 클릭 시 환율 계산 실행
+    const convertButton = document.getElementById('convertButton');
+    convertButton.addEventListener('click', updateExchangeRate);
 
-    if (isNaN(amount)) {
-      setResult('Please enter a valid amount');
-      return;
-    }
-
-    const rate = await getExchangeRate(fromCurrency, toCurrency);
-    const convertedAmount = (amount * rate).toFixed(2);
-    setResult(`${amount} ${fromCurrency} = ${convertedAmount} ${toCurrency}`);
-  } catch (error) {
-    console.error('Error converting currency:', error);
-    setResult('Error converting currency. Please try again later.');
-  }
-};
-
-const setCurrencyOptions = async () => {
-  try {
-    const currencies = await getAllCurrencies();
-    const fromCurrencySelect = document.getElementById('fromCurrency');
-    const toCurrencySelect = document.getElementById('toCurrency');
-    const amountInput = document.getElementById('amount');
-    amountInput.addEventListener('keypress', (event) => {
+    // Amount 입력창에서 Enter 키 입력 시 환율 계산 실행
+    amountInput.addEventListener('keypress', event => {
       if (event.key === 'Enter') {
-        event.preventDefault(); // Prevent form submission on Enter key press
-        convertCurrency(); // Trigger the convertCurrency function
+        event.preventDefault();
+        updateExchangeRate();
       }
     });
 
-    Object.keys(currencies).forEach(currency => {
-      const option = document.createElement('option');
-      option.text = currency;
-      option.value = currency;
-      fromCurrencySelect.appendChild(option);
-    });
-
-    Object.keys(currencies).forEach(currency => {
-      const option = document.createElement('option');
-      option.text = currency;
-      option.value = currency;
-      toCurrencySelect.appendChild(option);
-    });
-
-    fromCurrencySelect.value = 'SGD';
-    toCurrencySelect.value = 'KRW';
-  } catch (error) {
-    console.error('Error loading currency options:', error);
-  }
-};
-
-const setResult = (message) => {
-  document.getElementById('result').innerText = message;
-};
-
-const convertButton = document.getElementById('convertButton');
-convertButton.addEventListener('click', convertCurrency);
-
-setCurrencyOptions();
+    // 페이지 로딩 시 초기 환율 계산 실행
+    updateExchangeRate();
+  })
+  .catch(error => console.error(error));
